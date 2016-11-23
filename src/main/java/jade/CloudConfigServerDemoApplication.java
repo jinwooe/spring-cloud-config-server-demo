44f396be114960bb6c3df0e9d4f493610703deb7
Jade Im <jadeim@Jadeui-MacBook-Pro.local>
2016-11-23 16:11:24 +0900

    first commit

---
+++ src/main/java/jade/CloudConfigServerDemoApplication.java
@@ -0,0 +1,14 @@
+package jade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class CloudConfigServerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigServerDemoApplication.class, args);
+	}
+}
